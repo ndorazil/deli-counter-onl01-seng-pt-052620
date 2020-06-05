6cae75c6aafa256213e4bddfb07cb1ab00a0134c
Nathan Dorazil <nathand1915@gmail.com>
2020-06-05 00:23:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ def now_serving(line_array)
     puts "There is nobody waiting to be served!"
   else
     line_array.each do |customer_served|
-      line_array.puts "Currently serving #{customer_served}."
+      puts "Currently serving #{customer_served.first}."
       line_array.pop
     end
   end
