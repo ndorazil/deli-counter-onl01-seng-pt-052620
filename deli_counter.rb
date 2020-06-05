2b002807b4e63e6a8f1ad83d7f047c2f1f9cf122
Nathan Dorazil <nathand1915@gmail.com>
2020-06-05 00:09:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,8 @@ def now_serving(line_array)
   if line_array.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts line(line_array)
+    line(line_array) = line_array
+    line_array.shift
   end
 end
     
